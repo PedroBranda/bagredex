287cc82fb086f9cef193d3b623a7df88b02da54a
PedroBranda <pedrohbspg@gmail.com>
2024-11-24 17:18:03 -0300

    feat: changing the port and host configuration, changing from npm to yarn package manager

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+    watch: {
+      usePolling: true
+    }
+  }
 });
